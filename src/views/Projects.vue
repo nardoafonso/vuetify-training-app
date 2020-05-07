884fbd7fe14ffd5ad520a0b10aa5c1f45c559d3a
Leonardo Augusto Souza Afonso <nardoasafonso@gmail.com>
2020-05-07 00:36:07 -0300

    project set up, adding some views, navigation drawer, appbar, grid system, tooltips, buttons, container, margin and padding

---
+++ src/views/Projects.vue
@@ -0,0 +1,9 @@
+<template>
+ <div class="projects">
+    <h1 class="subtitle-1 grey--text">Projects</h1>
+    <v-container class="my-5">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus perferendis dolorem officia, aut exercitationem sint consequatur velit dolor accusamus, ut modi delectus, quae voluptas facere ratione. Pariatur perferendis in aspernatur.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus perferendis dolorem officia, aut exercitationem sint consequatur velit dolor accusamus, ut modi delectus, quae voluptas facere ratione. Pariatur perferendis in aspernatur.</p>
+    </v-container>
+ </div>
+</template>
---
+++ src/views/Team.vue
@@ -0,0 +1,9 @@
+<template>
+ <div class="team">
+    <h1 class="subtitle-1 grey--text">Team</h1>
+    <v-container class="my-5">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus perferendis dolorem officia, aut exercitationem sint consequatur velit dolor accusamus, ut modi delectus, quae voluptas facere ratione. Pariatur perferendis in aspernatur.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus perferendis dolorem officia, aut exercitationem sint consequatur velit dolor accusamus, ut modi delectus, quae voluptas facere ratione. Pariatur perferendis in aspernatur.</p>
+    </v-container>
+ </div>
+</template>
